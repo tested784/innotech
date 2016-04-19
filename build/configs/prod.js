401f32037ec34cdda13380b28155be5be635ed10
Kevin Damstra <kevin@notredamstra.com>
2016-04-19 11:36:02 +0200

    fixed svg + landscape hide h2

--- build/configs/prod.js
+++ build/configs/prod.js
@@ -28,10 +28,6 @@ config.module.loaders.push(
     test: /\.html$/,
     loader: 'handlebars'
   },
-    {
-      test   : /\.(ttf|eot|svg|woff(2))(\?[a-z0-9]+)?$/,
-      loader : 'file-loader'
-    },
   {
     test: [/\.scss$/, /\.sass$/],
     loader: ExtractTextPlugin.extract("style-loader", "css-loader?minimize!postcss-loader!sass-loader")
